60f2810e965d138164b334e9cb1926fa053e4a79
VladVasilegin <Vasilegin@gmail.com>
2022-12-04 15:12:57 +0300

    Created two projects frontend-react and backend-kotlin

---
+++ adventurer/src/main/kotlin/com/guild/kotlin/adventurer/AdventurerApplication.kt
@@ -0,0 +1,11 @@
+package com.guild.kotlin.adventurer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AdventurerApplication
+
+fun main(args: Array<String>) {
+	runApplication<AdventurerApplication>(*args)
+}
---
+++ adventurer/src/test/kotlin/com/guild/kotlin/adventurer/AdventurerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.guild.kotlin.adventurer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AdventurerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
